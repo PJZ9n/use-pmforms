afd3f327a9d4e8d5965ca8ffb6838b0c720314cd
PJZ9n <pjz9n@yahoo.co.jp>
2020-06-17 08:58:41 +0900

    コマンド実装

--- src/PJZ9n/UsePmforms/Main.php
+++ src/PJZ9n/UsePmforms/Main.php
@@ -23,9 +23,19 @@ declare(strict_types=1);
 
 namespace PJZ9n\UsePmforms;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
-    //
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        switch ($command->getName()) {
+            case "pmforms":
+                //
+                return true;
+        }
+        return false;
+    }
 }
