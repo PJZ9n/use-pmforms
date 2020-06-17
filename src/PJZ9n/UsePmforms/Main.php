05595344e49f45f973c7b914441e918797572368
PJZ9n <pjz9n@yahoo.co.jp>
2020-06-17 09:19:55 +0900

    フォーム実装

--- src/PJZ9n/UsePmforms/Main.php
+++ src/PJZ9n/UsePmforms/Main.php
@@ -23,9 +23,17 @@ declare(strict_types=1);
 
 namespace PJZ9n\UsePmforms;
 
+use dktapps\pmforms\CustomForm;
+use dktapps\pmforms\CustomFormResponse;
+use dktapps\pmforms\element\Dropdown;
+use dktapps\pmforms\element\Input;
+use dktapps\pmforms\element\Slider;
+use dktapps\pmforms\element\StepSlider;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase
 {
@@ -33,7 +41,32 @@ class Main extends PluginBase
     {
         switch ($command->getName()) {
             case "pmforms":
-                //
+                if (!$sender instanceof Player) {
+                    $sender->sendMessage(TextFormat::RED . "このコマンドはゲーム内から実行してください。");
+                    return true;
+                }
+                $element1 = new Dropdown("rate", "サーバーの評価", ["とても良い", "良い", "悪い", "とても悪い"], 0);
+                $element2 = new StepSlider("sns", "好きなSNS", ["Instagram", "LINE", "Twitter"], 2);
+                $sender->sendForm(new CustomForm(
+                    "テストフォーム",
+                    [
+                        new Input("name", "名前", "Steve", "Alex"),
+                        new Input("food", "好きな食べ物", "Apple", "Banana"),
+                        $element1,
+                        new Slider("age", "年齢", 0, 250, 1, (float)50),//第6引数がfloatを要求するため
+                        $element2,
+                    ],
+                    function (Player $player, CustomFormResponse $response) use ($element1, $element2): void {
+                        var_dump($response->getAll());
+                        $text = "";
+                        $text .= "名前: {$response->getString("name")}\n";
+                        $text .= "好きな食べ物: {$response->getString("food")}\n";
+                        $text .= "サーバーの評価: {$element1->getOption($response->getInt("rate"))}\n";
+                        $text .= "年齢: {$response->getInt("age")}\n";
+                        $text .= "好きなSNS: {$element2->getOption($response->getInt("sns"))}\n";
+                        $player->sendMessage($text);
+                    }
+                ));
                 return true;
         }
         return false;
